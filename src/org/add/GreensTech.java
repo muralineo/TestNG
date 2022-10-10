2984f20c70405e2dfb9b66b788317b86fa144b6a
muralineo <murali4neo@gmail.com>
2022-10-10 06:26:30 +0530

    First Commit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr() {
+		System.out.println("OMR");
+	}
+	
+	public void greensAdayar() {
+		System.out.println("Adayar");
+	}
+	
+	public void greensTambaram() {
+		System.out.println("Tambaram");
+	}
+	
+	public void greensVelachery() {
+		System.out.println("Velachery");
+	}
+	
+	public void greensAnnaNagar() {
+		System.out.println("AnnaNagar");
+	}
+	
+	public static void main(String[] args) {
+		
+		GreensTech branch = new GreensTech();
+		branch.greensOmr();
+		branch.greensAdayar();
+		branch.greensTambaram();
+		branch.greensVelachery();
+		branch.greensAnnaNagar();
+	
+	}
+}
